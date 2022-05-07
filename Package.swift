c624db12d5a5eff150c96c0cdfcfb3e7e88075d3
mtgto <hogerappa@gmail.com>
2022-05-07 13:07:43 +0900

    Set binaryTarget

--- Package.swift
+++ Package.swift
@@ -18,11 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ExampleSwiftPackage",
-            dependencies: []),
-        .testTarget(
-            name: "ExampleSwiftPackageTests",
-            dependencies: ["ExampleSwiftPackage"]),
+        // .target(name: "ExampleSwiftPackage", dependencies: [])
+        .binaryTarget(name: "ExampleSwiftPackage", url: "https://github.com/mtgto/ExampleSwiftPackage/releases/download/0.1.0/ExampleSwiftPackage.zip", checksum: "addb0ef4f637f2d005cda700d2b2104a1907fd1ee7addd8d6185d8b94c0f1cd3")
     ]
 )
